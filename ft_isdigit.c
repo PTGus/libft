c80d84ebf01c6f0027221b2306f59f4caa93d854
Gustavo Mendes Da Cunha <gumendes@c1r5s5.42porto.com>
2025-07-08 11:44:30 +0100

    Clarification to th comments

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gumendes <gumendes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gumendes <gumendes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:02:34 by gumendes          #+#    #+#             */
-/*   Updated: 2024/10/29 14:20:07 by gumendes         ###   ########.fr       */
+/*   Updated: 2025/07/08 11:43:47 by gumendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 /** @brief See if it is a digit (0-9)
- *  @param c - character
- *  @return returns 1 if not found and 0 if found
+ *  @param c  Character
+ *  @return Returns 1 if not found and 0 if found
 */
 int	ft_isdigit(int c)
 {
